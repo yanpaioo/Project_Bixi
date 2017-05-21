dfa2a87695b50e3d421088dd198dd687bae990bd
yanpaioo <yanpaioo94@gmail.com>
2017-05-21 23:02:41 +0800

    sensor test for linear guide

--- bixi_arduino/sensor_test/sensor_test.ino
+++ bixi_arduino/sensor_test/sensor_test.ino
@@ -1,7 +1,11 @@
-const int sensor1 = A5;
-const int sensor2 = 41;
+const int sensor1 = 10; // upper sensor
+const int sensor2 = A9; // lower sensor
+
+
 const int power1 = A8;
-const int power2 = 33;
+const int power2 = A11;
+const int grd2 = A14;
+
 bool value1, value2;
 
 void setup() {
@@ -10,8 +14,10 @@ void setup() {
   pinMode(sensor2, INPUT);
   pinMode(power1, OUTPUT);
   pinMode(power2, OUTPUT);
-  digitalWrite(power1, HIGH);
+  pinMode(grd2, OUTPUT);
+  // digitalWrite(power1, HIGH);
   digitalWrite(power2, HIGH);
+  digitalWrite(grd2, LOW);
   
   Serial.begin(9600);
 }
@@ -24,5 +30,5 @@ void loop() {
   Serial.print("\tSensor2 - ");
   Serial.print(value2);
   Serial.println();
-  delay(100);
+  delay(500);
 }
